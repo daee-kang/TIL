7ea37cebd163e081d5c6ab521e62170489f7089f
daee kang <44410043+daeeKang@users.noreply.github.com>
2020-10-06 14:31:07 -0700

    GET /headers/:category/:page

--- routes/api.js
+++ routes/api.js
@@ -8,10 +8,10 @@ const marked = require('marked')
 /* override marked renderer to match client side (dumbed down) */
 const renderer = {
   heading(text, level) {
-      return `<h${level} class="marked" id="blahblahblah">${text}</h${level}>`;
+    return `<h${level} class="marked" id="blahblahblah">${text}</h${level}>`;
   }
 };
-marked.use({renderer})
+marked.use({ renderer })
 //---------------------------------------------------------------------------------
 
 //return all
@@ -25,12 +25,12 @@ router.get('/', (req, res, next) => {
 router.post('/:category', async (req, res, next) => {
   const { category } = req.params
 
-  const data = await Category.find({name: category})
-  if(data.length != 0) {
+  const data = await Category.find({ name: category })
+  if (data.length != 0) {
     res.json("already exists")
     return
   }
-  
+
   const c = new Category({
     _id: new mongoose.Types.ObjectId(),
     name: category,
@@ -52,13 +52,15 @@ router.post('/:category/:page', (req, res, next) => {
   const { category, page } = req.params
   console.log(category, page)
 
-  Category.updateOne({name: category}, {$addToSet: {
-    "pages": {
-      page: page,
-      text: ""
+  Category.updateOne({ name: category }, {
+    $addToSet: {
+      "pages": {
+        page: page,
+        text: ""
+      }
     }
-  }}, (err, data) => {
-    if(err) console.log(err) 
+  }, (err, data) => {
+    if (err) console.log(err)
     res.json(data)
   })
 })
@@ -66,17 +68,19 @@ router.post('/:category/:page', (req, res, next) => {
 //update page in category
 router.put('/:category/:page', (req, res, next) => {
   const { category, page } = req.params
+  console.log('puttINGGG')
   console.log(category, page)
+  console.log(req.body.text)
 
-  Category.findOne({name: category}, (err, data) => {
-    if(data === null) {
+  Category.findOne({ name: category }, (err, data) => {
+    if (data === null) {
       res.json("category doesn't exist")
       return
     }
 
     console.log(data);
-    for(let i = 0; i < data.pages.length; i++) {
-      if(data.pages[i].page === page) {
+    for (let i = 0; i < data.pages.length; i++) {
+      if (data.pages[i].page === page) {
         data.pages[i].text = req.body.text
       }
     }
@@ -99,14 +103,15 @@ router.get('/:category/:page', (req, res, next) => {
 
   Category.findOne({ name: category, "pages.page": page })
     .then(results => {
-      if(results === null) {
+      if (results === null) {
         res.json("not found")
         return
       }
 
-      for(let i = 0; i < results.pages.length; i++) {
-        if(results.pages[i].page === page) {
+      for (let i = 0; i < results.pages.length; i++) {
+        if (results.pages[i].page === page) {
           res.json(results.pages[i])
+          return
         }
       }
     })
@@ -117,30 +122,30 @@ router.get('/:category/:page', (req, res, next) => {
 
 router.get('/menuItems', (req, res, next) => {
   Category.find({})
-  .then((data) => {
-    if(data === null) {
-      res.json([])
-      return
-    }
+    .then((data) => {
+      if (data === null) {
+        res.json([])
+        return
+      }
 
-    let out = []
-    data.forEach((category) => {
-      let outItem = {}
-      outItem.title = category.name
+      let out = []
+      data.forEach((category) => {
+        let outItem = {}
+        outItem.title = category.name
 
-      let outPages = []
-      category.pages.forEach(page => {
-        outPages.push(page.page)
+        let outPages = []
+        category.pages.forEach(page => {
+          outPages.push(page.page)
+        })
+
+        outItem.pages = outPages
+
+        out.push(outItem)
       })
 
-      outItem.pages = outPages
-
-      out.push(outItem)
+      res.json(out)
     })
-
-    res.json(out)
-  })
-  .catch(next)
+    .catch(next)
 })
 
 router.get('/allHeaders', (req, res, next) => {
@@ -150,22 +155,61 @@ router.get('/allHeaders', (req, res, next) => {
     texts.forEach(t => {
       let markedText = marked(t.text)
       console.log(markedText)
-      const regExpr = /<h[12] class=\"marked\" [^>]+>(.*?)<\/h[12]>/g 
+      const regExpr = /<h[12] class=\"marked\" [^>]+>(.*?)<\/h[12]>/g
       //const regExpr = /<h[12] [^>]+>(.*?)<\/h[12]>/g
 
       let m
       let regArray = []
-      do{
+      do {
         m = regExpr.exec(markedText)
-        if(m) {
+        if (m) {
           regArray.push(m[1])
         }
-      } while(m)
+      } while (m)
 
       userMap[t._id] = regArray
     })
 
     res.send(userMap)
+  })
+})
+
+router.get('/headers/:category/:page', (req, res, next) => {
+  const { category, page } = req.params
+
+  Category.findOne({ name: category, "pages.page": page }, (err, data) => {
+    if (err) {
+      res.json(err)
+      return
+    }
+
+    if (data === null) {
+      res.json({})
+      return
+    }
+
+    data.pages.forEach((p) => {
+      if (p.page === page) {
+        let markedText = marked(p.text)
+        console.log(markedText)
+        const regExpr = /<h[23] class=\"marked\" [^>]+>(.*?)<\/h[23]>/g
+        //const regExpr = /<h[12] [^>]+>(.*?)<\/h[12]>/g
+
+        let m
+        let regArray = []
+        do {
+          m = regExpr.exec(markedText)
+          if (m) {
+            regArray.push(m[1])
+          }
+        } while (m)
+
+        res.json(regArray)
+        return
+      }
+    })
+
+    res.json('hmm')
   })
 })
 
