9306a2fe9e1bdaec81ad8a9a56526b35c6473d00
daee <daee@unlv.nevada.edu>
2020-10-08 22:38:21 -0700

    work on home page

--- client/src/components/home/home.js
+++ client/src/components/home/home.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import './home.scss'
+import '../markdownDisplay/markdownDisplay.scss'
 
 const Home = () => {
     return (
-        <div>
-            dab on me bruh
+        <div id="home">
+            <div className="md-content">
+                <h1>daee's dev journal</h1>
+                These are just my dev notes :-)
+                
+                <div id="add-page">
+                    <h2>add a new category:</h2>
+                    <input></input>
+                </div>
+
+                <div id="add-page">
+                    <h2>add a new page:</h2>
+                    
+                </div>
+            </div>
         </div>
     )
 }
