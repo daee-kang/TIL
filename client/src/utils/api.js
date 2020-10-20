c01fe21eff5baa9a128a1099fa4666ea40581d13
daee <daee@unlv.nevada.edu>
2020-10-19 23:12:53 -0700

    delete front end

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,6 +2,6 @@ import axios from 'axios'
 
 export default axios.create({
     //todo: create env variable for this
-    baseURL: "http://localhost:5000/api",
+    baseURL: "https://svzz83ygq6.execute-api.us-east-1.amazonaws.com/dev/api",
     responseType: "json"
 })
